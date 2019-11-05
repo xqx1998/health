70649ffe9723cebf957a92fcb9237190033c7abf
xqx1998 <2515658674@qq.com>
2019-11-05 12:02:06 +0800

    环境搭建 准备工作

--- health_parent/health_common/src/main/java/com/eight/group/pojo/User.java
+++ health_parent/health_common/src/main/java/com/eight/group/pojo/User.java
@@ -1,14 +1,23 @@
 package com.eight.group.pojo;
-import	java.io.Serializable;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * @author：xingquanxiang createTime：2019/11/3 20:57
- * description:
+ * 用户
  */
 public class User implements Serializable{
-    private Integer id;
-    private String username;
-    private String password;
+    private Integer id; // 主键
+    private Date birthday; // 生日
+    private String gender; // 性别
+    private String username; // 用户名，唯一
+    private String password; // 密码
+    private String remark; // 备注
+    private String station; // 状态
+    private String telephone; // 联系电话
+    private Set<Role> roles = new HashSet<Role>(0);//对应角色集合
 
     public Integer getId() {
         return id;
@@ -16,6 +25,22 @@ public class User implements Serializable{
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public String getUsername() {
@@ -34,12 +59,35 @@ public class User implements Serializable{
         this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
